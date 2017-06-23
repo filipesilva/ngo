0b4581d5922d36804a81bda8dea866ac7992e810
Filipe Silva <filipematossilva@gmail.com>
2017-06-23 10:58:07 +0100

    test: add manual prod mode

--- integration/webpack-app/webpack.config.common.js
+++ integration/webpack-app/webpack.config.common.js
@@ -45,8 +45,19 @@ module.exports = {
       threshold: 0,
       minRatio: 0.8
     }),
+    new webpack.HashedModuleIdsPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
     new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
       mangle: { screw_ie8: true },
       compress: { screw_ie8: true, warnings: false, pure_getters: true },
       sourceMap: true,
