6d40042689b146fe58e60408ea59c941065b2f7a
Filipe Silva <filipematossilva@gmail.com>
2017-06-23 13:07:28 +0100

    test: remove LoaderOptionsPlugin
    It's deprecated.

--- integration/webpack/aio-app/webpack.config.common.js
+++ integration/webpack/aio-app/webpack.config.common.js
@@ -52,10 +52,6 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
     new webpack.optimize.UglifyJsPlugin({
       beautify: false,
       mangle: { screw_ie8: true },
